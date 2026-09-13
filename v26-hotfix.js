fdd05cd7496a886b86bf997910cf0c91b22aafb0
Xy Tolentino <hero.tolentino@gmail.com>
2026-09-13 20:16:12 +0800

    Load rail auth control

--- v26-hotfix.js
+++ v26-hotfix.js
@@ -1,6 +1,7 @@
 import './v26-core.js';
 import './v27-patch.js';
 import './supabase-bridge.js';
+import './rail-auth.js';
 import './supabase-normalized.js';
 import './supabase-reward-log.js';
 import './v34-guest-avatar.js';
